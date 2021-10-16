c6a8b78d9d475c07416a473886c1a359928b37a0
kargayerde <51271164+kargayerde@users.noreply.github.com>
2021-10-16 05:02:37 +0300

    websocket with gasgas.io API

--- gasalarm.js
+++ gasalarm.js
@@ -1,60 +1,76 @@
 import sound from "sound-play";
-import etherscanApi from "etherscan-api";
 import prompt from "prompt-sync";
+import WebSocket from "ws";
 
 const filePath = new URL("./asdf.wav", import.meta.url).href;
-
-var api = etherscanApi.init(""); // enter etherscan api key if you have one.
-
-const pollRate = 30; // polling rate in seconds
-
-const convHexWeiToGwei = (hexWei) => parseInt(hexWei, 16) / 10 ** 9;
-
-const getGasPrice = async () => {
-	let gasPriceData = await api.proxy.eth_gasPrice();
-	let gweiGas = convHexWeiToGwei(gasPriceData.result);
-	console.log(`gas price: ${gweiGas} gwei`);
-	return gweiGas;
-};
-
-const checkLoop = (alarm) => {
-	let pass = 0;
-
-	const requestPrice = async () => {
-		pass += 1;
-
-		console.log(`sending request ${pass}`);
-		let price = await getGasPrice();
-
-		if (price <= alarm) {
-			console.log(
-				`=============== gas price is ${price} gwei, ${
-					alarm - price
-				} cheaper than alarm`
-			);
-
-			sound.play(filePath, 1);
-		}
-	};
-
-	requestPrice();
-	setInterval(() => requestPrice(), pollRate * 1000);
-};
+const convWeiToGwei = (wei) => wei / 10 ** 9;
+const parseTime = (e) => new Date(e);
 
 const main = () => {
 	console.log("enter desired gas price for alarm or skip");
 	var alarmCheck = new prompt();
-	const treshold = parseInt(alarmCheck("gas price in gwei: "));
+	const threshold = parseInt(
+		alarmCheck("gas price in gwei (standard speed): ")
+	);
+	let alarm;
 
-	if (!Number.isInteger(treshold) || treshold === 0) {
+	if (!Number.isInteger(threshold) || threshold === 0) {
 		console.log("running without alarm...");
-		checkLoop(0);
+		alarm = false;
 	} else {
 		console.log(
-			`alarm set at ${treshold} gwei (polling rate: ${pollRate} sec.)`
+			`alarm set at ${threshold} gwei`
 		);
-		checkLoop(treshold);
+		alarm = true;
 	}
+
+	const socket = new WebSocket("wss://gasgas.io/prices");
+
+	socket.onopen = (e) => {
+		// console.log("connected", {e});
+	};
+
+	socket.onmessage = (e) => {
+		let prices = Object.values(JSON.parse(e.data).data).map(
+			(value, index) => {
+				return index === 4
+					? parseTime(value)
+					: convWeiToGwei(value).toFixed(1);
+			}
+		);
+
+		console.log(`Data received at ${prices[4]}`);
+
+		console.log(
+			`rapid: ${prices[0]}\t|\tfast: ${prices[1]}\t|\tstandard: ${prices[2]}\t|\tslow: ${prices[3]}\t|\t`
+		);
+
+		if (alarm) {
+			if (prices[2] <= threshold) {
+				console.log(
+					`=============== gas price is ${prices[2]} gwei, ${
+						threshold - prices[2]
+					} cheaper than alarm`
+				);
+
+				sound.play(filePath, 1);
+			}
+		}
+	};
+
+	socket.onclose = (e) => {
+		if (e.wasClean) {
+			console.log(
+				`[close] Connection closed cleanly, code=${e.code} reason=${e.reason}`
+			);
+		} else {
+			console.log("[close] Connection died");
+		}
+	};
+
+	socket.onerror = (e) => {
+		console.log({ e: e.message });
+	};
 };
 
 main();
